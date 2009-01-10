236994eadf8c135e5db568fc5336a7c3f5872005
Giovanni Dall'Olio <dalloliogm@gmail.com>
2009-01-10 14:16:50 +0100

    first skeleton for the bot

---
+++ testbot/bot.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# python script by Giovanni Dall'Olio
+"""
+a bot to test some website I need to test
+
+"""
+import logging
+from config import url, username, password
+
+
+
+def _test():
+	"""test the current module with doctest"""
+	import doctest
+	doctest.testmod()
+
+if __name__ == '__main__':
+	_test()
+
