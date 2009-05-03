7ae29ccf1605f9fe7168920393ed888a36557537
Giovanni Dall'Olio <dalloliogm@gmail.com>
2009-05-03 10:15:35 +0200

    login function

--- testbot/bot.py
+++ testbot/bot.py
@@ -8,6 +8,27 @@ import logging
 from config import url, username, password
 
 
+def login(br):
+	"""
+	log to the forum.
+
+	Input:
+	- the browser object
+	"""
+	# select the login form, which is the only one available from the main page
+	br.select_form(nr=0)
+
+	br['loginUsername'] = username
+	br['loginPassword'] = password
+
+	resp = br.submit()
+
+
+
+
+
+
+
 
 def _test():
 	"""test the current module with doctest"""
